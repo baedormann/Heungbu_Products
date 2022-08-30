a9198b0d2ddc9f5fc0ce67b5a284d27fc264aee9
Bae Dohun <baedormann@gmail.com>
2022-08-30 11:06:15 +0900

    login view 구현(미완)

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,7 @@ const router = express.Router();
 /* GET login page. */
 router.get('/', function(req, res, next) {
     res.render('login');
+    //res.render('testPage');
 });
 
 module.exports = router;
