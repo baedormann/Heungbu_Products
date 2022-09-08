aea66c912ac13c3aa30600caa4fcf638a7651fbb
vo0922 <71069665+vo0922@users.noreply.github.com>
2022-09-08 13:53:33 +0900

    product modal

--- public/javascripts/productModal/productDetail.js
+++ public/javascripts/productModal/productDetail.js
@@ -10,12 +10,36 @@ function productModalOpen(product_code) {
             "Authorization": "Bearer " + getCookie('token')
         }
     }).then(response => response.json()).then((data) => {
-        console.log(data);
         document.getElementById("product_name").innerHTML = `물품 이름 : ${data.product_name}`
         document.getElementById("product_category").innerHTML = `대분류 : ${data.product_category.firstCategory} 소분류 : ${data.product_category.secondCategory}`
         document.getElementById("product_code").innerHTML = `물품코드 : ${data.product_code}`
         document.getElementById("rental_availability").innerHTML = `대여 여부 : ${data.rental_availability}`
         document.getElementById("return_needed").innerHTML = `반납 여부 : ${data.return_needed}`
         document.getElementById("quantity").innerHTML = `물품 수량 : ${data.quantity}`
+        document.getElementById("inputStart").innerHTML = `시작일 : <input id="startDate" type="datetime-local" onchange="startEnd()">`
+        document.getElementById("inputEnd").innerHTML = `종료일 : <input id="endDate" type="datetime-local" onchange="startEnd()">`
+        document.getElementById("inputText").innerHTML = `대여 목적 : <input type="text">`
+        defaultStartEnd();
     });
+}
+
+// 시작일 종료일 기본값
+function defaultStartEnd() {
+    let startElement = document.getElementById('startDate');
+    let endElement = document.getElementById('endDate');
+    let date = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().slice(0, -8);
+    console.log(date)
+    startElement.value = date;
+    startElement.min = date;
+    endElement.min = date;
+}
+
+// 시작일 종료일
+function startEnd() {
+    let start = document.getElementById('startDate');
+    let end = document.getElementById('endDate');
+        if (start.value)
+            end.min = start.value;
+        if (end.value)
+            start.max = end.value;
 }
