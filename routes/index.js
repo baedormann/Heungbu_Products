1f6129157c6e50eedf58a6d5450b4b8e5a534036
vo0922 <71069665+vo0922@users.noreply.github.com>
2022-09-21 19:47:13 +0900

    main date update

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/', authUtil, async function (req, res, next) {
     let rentals;
     let members;
     if(decode.manage||decode.open_auth){
-        rentals = await rental.find().sort({rental_date: -1}).limit(5)
+        rentals = await rental.find({rental_status:"대여중"}).sort({rental_date: -1}).limit(5)
             .populate('product_id')
             .populate({
                 path: 'emp_id',
@@ -23,7 +23,7 @@ router.get('/', authUtil, async function (req, res, next) {
     if(decode.manage) members = await member.find().sort({emp_no: 1}).limit(5);
     const products = await product.find().sort({last_date: -1}).limit(5);
     const myInfo = await member.findOne({emp_no: decode.emp_no});
-    const myRent = await rental.find({emp_id: myInfo._id}).sort({rental_date: -1}).limit(5).populate('product_id');
+    const myRent = await rental.find({emp_id: myInfo._id, rental_status: "대여중"}).sort({rental_date: -1}).limit(5).populate('product_id');
     const myRentCount = await rental.find({emp_id: myInfo._id, rental_status: "대여중" }).count();
     const myReturnCount = await rental.find({emp_id: myInfo._id, rental_status: "반납" }).count().populate('product_id');
     const productCount = await product.find().count();
