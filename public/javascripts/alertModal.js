a18c716c34d402772923ddc7b8c09756ebe391fb
Bae Dohun <baedormann@gmail.com>
2022-09-28 16:25:38 +0900

    모달창 수정

--- public/javascripts/alertModal.js
+++ public/javascripts/alertModal.js
@@ -1,9 +1,13 @@
-function alert(text){
+function alert(text, reload){
     let alertModal = document.getElementById('alertModal');
     alertModal.style.display = 'block';
-    document.getElementsByClassName('alertText')[0].innerHTML = text;
+
+    if(reload){
+        document.getElementsByClassName('alertBtnDiv')[0].innerHTML = '<button id="okBtn" onclick="alertDone(true)">확인</button>';
+    }
+    document.getElementsByClassName('alertText')[0].innerText = text;
 }
-function alertDone(){
+function alertDone(reload){
     document.getElementById('alertModal').style.display = 'none';
-    return true;
+    reload ? location.reload() : true;
 }
